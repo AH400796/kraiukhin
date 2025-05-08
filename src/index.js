a6657243ec0cf2d5f6b57bd2307d91a6549bf985
Andrii Hokhman <A.Hokhman@gmail.com>
2025-05-08 23:07:02 +0300

    333

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/oleh-kraiukhin">
+    <BrowserRouter basename="/kraiukhin">
       <App />
     </BrowserRouter>
   </React.StrictMode>
