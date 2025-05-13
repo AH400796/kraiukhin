3f5dc72fad614dc758337cde8f3fcb51ad5b56f3
Andrii Hokhman <A.Hokhman@gmail.com>
2025-05-13 21:36:32 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -10,7 +10,7 @@ export default function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<Navigate to="/about" replace={true} />} />
+        {/* <Route path="/" element={<Navigate to="/about" replace={true} />} /> */}
         <Route path="/" element={<SharedLayout />}>
           <Route path="about" element={<Home />} />
           <Route path="books" element={<Books />} />
