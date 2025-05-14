3fed1531bea1e3b3f0cdc845e085ad9540d6fb6c
Andrii Hokhman <A.Hokhman@gmail.com>
2025-05-14 19:08:46 +0300

    zxc

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,19 +3,19 @@ import { lazy } from 'react';
 import SharedLayout from 'components/SharedLayout/SharedLayout';
 
 const Home = lazy(() => import('../pages/Home'));
-// const Books = lazy(() => import('../pages/Books'));
-// const Contacts = lazy(() => import('../pages/Contacts'));
+const Books = lazy(() => import('../pages/Books'));
+const Contacts = lazy(() => import('../pages/Contacts'));
 
 export default function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={<SharedLayout />}>
-          <Route index element={<Navigate to="/about" replace />} />
+          <Route index element={<Navigate to="about" replace />} />
           <Route path="about" element={<Home />} />
-          {/* <Route path="books" element={<Books />} />
+          <Route path="books" element={<Books />} />
           <Route path="contacts" element={<Contacts />} />
-          <Route path="*" element={<Navigate to="about" replace={true} />} /> */}
+          <Route path="*" element={<Navigate to="about" replace />} />
         </Route>
       </Routes>
     </>
